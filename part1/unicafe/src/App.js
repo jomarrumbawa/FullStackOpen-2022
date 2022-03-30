56403cdc819880a2f7acbdf3ce594a25c609208e
Jomar Rumbawa <me@jomarrumbawa.com>
2022-03-30 10:59:49 -0700

    create functions and display statistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return <div>code here</div>
+  const handleGood = () => {
+    setGood((prevState) => prevState + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral((prevState) => prevState + 1)
+  }
+
+  const handleBad = () => {
+    setBad((prevState) => prevState + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
 }
 
 export default App
