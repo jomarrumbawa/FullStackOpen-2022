fd8f34a46693c8ef061550d96f59aa66c8f93b05
Jomar Rumbawa <me@jomarrumbawa.com>
2022-04-01 13:33:21 -0700

    add phone number functionality

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (e) => {
     e.preventDefault()
     const newPerson = {
       name: newName,
+      number: newNumber,
     }
 
     persons.map((person) => {
@@ -18,6 +22,11 @@ const App = () => {
         setPersons(persons.concat(newPerson))
       }
     })
+  }
+
+  const handleNumberChange = (e) => {
+    console.log(e.target.value)
+    setNewNumber(e.target.value)
   }
 
   const handleNameChange = (e) => {
@@ -33,12 +42,17 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   )
