76ea8b66fc811973a0387951fe3be209fe00cc71
Jomar Rumbawa <me@jomarrumbawa.com>
2022-03-31 17:36:51 -0700

    add Total component and logic

--- part2/courseinfo-2/src/components/Course.js
+++ part2/courseinfo-2/src/components/Course.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const Course = ({ course }) => {
+  const arr = course.parts.map((part) => part.exercises)
+  console.log(arr)
+  const total = arr.reduce((prev, curr) => prev + curr)
+  console.log(total)
   return (
     <>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total total={total} />
     </>
   )
 }
