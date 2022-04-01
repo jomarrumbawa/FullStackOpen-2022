f29b968ee57127c7aa0b7738a753eb406eef6177
Jomar Rumbawa <me@jomarrumbawa.com>
2022-03-31 23:31:02 -0700

    add new person functionality

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,35 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
 
+  const addPerson = (e) => {
+    e.preventDefault()
+    const newPerson = {
+      name: newName,
+    }
+    setNewName('')
+    setPersons(persons.concat(newPerson))
+  }
+
+  const handleNameChange = (e) => {
+    console.log(e.target.value)
+    setNewName(e.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   )
 }
