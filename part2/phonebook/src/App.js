7b0bd71498c06a50465b489c4117a4593a59340a
Jomar Rumbawa <me@jomarrumbawa.com>
2022-04-05 09:37:27 -0700

    remove persons data

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,12 +4,7 @@ import AddPersonForm from './components/AddPersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filteredName, setFilteredName] = useState('')
@@ -45,10 +40,6 @@ const App = () => {
     console.log(e.target.value)
     setNewName(e.target.value)
   }
-
-  const filteredPersons = persons.filter((person) =>
-    person.name.toLowerCase().includes(filteredName)
-  )
 
   return (
     <div>
