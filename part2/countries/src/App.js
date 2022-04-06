0f63be7d8cdcdd8d06f72d8f3686779f7ce59826
Jomar Rumbawa <me@jomarrumbawa.com>
2022-04-06 12:07:59 -0700

    update App.js

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import SearchFilter from './components/SearchFilter'
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filteredName, setFilteredName] = useState('')
+
+  const hook = () => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((res) => setCountries(res.data))
+  }
+
+  useEffect(hook, [])
+
+  const handleFilteredNameChange = (e) => {
+    setFilteredName(e.target.value)
+  }
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(filteredName)
+  )
+
+  const countriesComponent =
+    filteredCountries.length < 10 ? (
+      filteredCountries.map((country) => (
+        <p key={country.name.common}>{country.name.common}</p>
+      ))
+    ) : (
+      <p>Too many countries, specify another filter</p>
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchFilter
+        filteredName={filteredName}
+        handleChange={handleFilteredNameChange}
+      />
+      {countriesComponent}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
